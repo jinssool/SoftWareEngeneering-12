b5f6a354c1b58c57ed99e27336e5f2ccdbe60799
jinssool <132546149+jinssool@users.noreply.github.com>
2023-12-06 11:36:14 +0000

    1

--- main2.c
+++ main2.c
@@ -1,115 +1,10 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#define XSIZE	5
-#define YSIZE	3
+#include "Project6.h"
 
-struct user_info {
-	int id_num;
-	char id[10];
-	char password[10];
-};
+
 char id[10], password[10];
 
-int load_user_info(struct user_info user[]);
-int check_login(struct user_info user[]);
-int print_seats(int s_array[][XSIZE]);
-int reserve(int s_array[][XSIZE], char r, int c, int id);
-int cancel(int s_array[][XSIZE], char r, int c, int id);
-int load_reservation_info(int s[][XSIZE]);
-int save_reservation_info(int s[][XSIZE]);
-
-int main(void)
-{
-	struct user_info user[10];
-	int c;
-	if ((c = load_user_info(user)) < 0)
-		fprintf(stderr, "error\n");
-	char row;
-	int C, id_num, col;
-	int select = 0, seat[YSIZE][XSIZE] = { 0 };
-	printf("�α����� �ϼ���.\n");
-	while (1)
-	{
-		printf("id: ");
-		scanf("%s", id);
-		printf("password: ");
-		scanf("%s", password);
-		C = check_login(user);
-		if (C == -1)
-			printf("�ش� ���̵� �����ϴ�. �ٽ� �α����� �ϼ���.\n");
-		else if (C == -2)
-			printf("�ش� ���̵�� �����ϳ� �н����尡 �ٸ��ϴ�. �ٽ� �α����� �ϼ���.\n");
-		else
-		{
-			id_num = C;
-			printf("%s�� �ݰ����ϴ�.\n", id);
-			break;
-		}
-	}
-	load_reservation_info(seat);
-
-	do
-	{
-		printf("1---�¼�Ȯ���ϱ�\n");
-		printf("2---�����ϱ�\n");
-		printf("3---��������ϱ�\n");
-		printf("4---�ٸ� ����ڷ� �α����ϱ�\n");
-		printf("5---�����ϱ�\n");
-		printf("�޴��� �����Ͻÿ�: ");
-		scanf("%d", &select);
-
-		if (select == 1)
-		{
-			printf("���õ� �޴� = �¼�Ȯ���ϱ�\n\n");
-			print_seats(seat);
-		}
-		else if (select == 2)
-		{
-			printf("���õ� �޴� = �����ϱ�\n\n");
-			printf("������ ���ϴ� �ڸ���(��-A2)? ");
-			scanf(" %c%d", &row, &col);
-			reserve(seat, row, col, id_num);
-			print_seats(seat);
-			save_reservation_info(seat);
-		}
-		else if (select == 3)
-		{
-			printf("���õ� �޴� = ��������ϱ�\n\n");
-			printf("������Ҹ� ���ϴ� �ڸ���(��-A2)? ");
-			scanf(" %c%d", &row, &col);
-			cancel(seat, row, col, id_num);
-			print_seats(seat);
-			save_reservation_info(seat);
-		}
-		else if (select == 4)
-		{
-			printf("���õ� �޴� = �ٸ� ����ڷ� �α����ϱ�\n\n");
-			printf("�α����� �ϼ���.\n");
-			while (1)
-			{
-				printf("id: ");
-				scanf("%s", id);
-				printf("password: ");
-				scanf("%s", password);
-				C = check_login(user);
-				if (C == -1)
-					printf("�ش� ���̵� �����ϴ�. �ٽ� �α����� �ϼ���.\n");
-				else if (C == -2)
-					printf("�ش� ���̵�� �����ϳ� �н����尡 �ٸ��ϴ�. �ٽ� �α����� �ϼ���.\n");
-				else
-				{
-					id_num = C;
-					printf("%s�� �ݰ����ϴ�.\n", id);
-					break;
-				}
-			}
-		}
-	} while (select != 5);
-	printf("���õ� �޴� = �����ϱ�\n\n");
-	printf("�̿��� �ּż� �����մϴ�.\n");
-	return 0;
-}
 
 int load_user_info(struct user_info user[])
 {
@@ -141,7 +36,8 @@ int load_user_info(struct user_info user[])
 	fclose(fp);
 	return i;
 }
-int check_login(struct user_info user[])
+
+int check_login(struct user_info user[], char* id, char* password)
 {
 	int low, high, middle;
 	low = 0;
@@ -185,12 +81,10 @@ int reserve(int s_array[][XSIZE], char r, int c, int num)
 		if (s_array[0][c] == 0)
 		{
 			s_array[0][c] = num;
-			printf("������ �Ϸ�Ǿ����ϴ�.\n");
 			return 1;
 		}
 		else
 		{
-			printf("�̹� ����� �ڸ��Դϴ�.\n");
 			return -1;
 		}
 	}
@@ -199,12 +93,10 @@ int reserve(int s_array[][XSIZE], char r, int c, int num)
 		if (s_array[1][c] == 0)
 		{
 			s_array[1][c] = num;
-			printf("������ �Ϸ�Ǿ����ϴ�.\n");
 			return 1;
 		}
 		else
 		{
-			printf("�̹� ����� �ڸ��Դϴ�.\n");
 			return -1;
 		}
 	}
@@ -213,12 +105,10 @@ int reserve(int s_array[][XSIZE], char r, int c, int num)
 		if (s_array[2][c] == 0)
 		{
 			s_array[2][c] = num;
-			printf("������ �Ϸ�Ǿ����ϴ�.\n");
 			return 1;
 		}
 		else
 		{
-			printf("�̹� ����� �ڸ��Դϴ�.\n");
 			return -1;
 		}
 	}
@@ -230,18 +120,15 @@ int cancel(int s_array[][XSIZE], char r, int c, int num)
 	{
 		if (s_array[0][c] == 0)
 		{
-			printf("������� ���� �ڸ��Դϴ�.\n");
 			return -1;
 		}
 		else if (s_array[0][c] == num)
 		{
 			s_array[0][c] = 0;
-			printf("������Ұ� �Ϸ�Ǿ����ϴ�.\n");
 			return 1;
 		}
 		else
 		{
-			printf("������Ҹ� �� �� �����ϴ�.\n");
 			return -2;
 		}
 	}
@@ -249,18 +136,15 @@ int cancel(int s_array[][XSIZE], char r, int c, int num)
 	{
 		if (s_array[1][c] == 0)
 		{
-			printf("������� ���� �ڸ��Դϴ�.\n");
 			return -1;
 		}
 		else if (s_array[1][c] == num)
 		{
 			s_array[1][c] = 0;
-			printf("������Ұ� �Ϸ�Ǿ����ϴ�.\n");
 			return 1;
 		}
 		else
 		{
-			printf("������Ҹ� �� �� �����ϴ�.\n");
 			return -2;
 		}
 	}
@@ -268,18 +152,15 @@ int cancel(int s_array[][XSIZE], char r, int c, int num)
 	{
 		if (s_array[2][c] == 0)
 		{
-			printf("������� ���� �ڸ��Դϴ�.\n");
 			return -1;
 		}
 		else if (s_array[2][c] == num)
 		{
 			s_array[2][c] = 0;
-			printf("������Ұ� �Ϸ�Ǿ����ϴ�.\n");
 			return 1;
 		}
 		else
 		{
-			printf("������Ҹ� �� �� �����ϴ�.\n");
 			return -2;
 		}
 	}
@@ -307,3 +188,4 @@ int save_reservation_info(int s[][XSIZE])
 	fclose(fp);
 	return i;
 }
+
